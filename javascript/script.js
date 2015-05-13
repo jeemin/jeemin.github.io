6bb77839ee373c115803fa01a1558a74057d1d2b
--replace-all <bj6kim@uwaterloo.ca>
2015-05-12 17:56:37 -0700

    replace duplicate experience

---
+++ javascript/script.js
@@ -0,0 +1,58 @@
+var inTransition = false;
+
+$(document).ready(function() {
+    $('._profile').click(function() {
+        if (!inTransition) {
+            if($('.resume').hasClass('hide')){
+            	$('.profile').removeClass('hide');
+            } else {
+        		inTransition = true;
+            	unrenderResume($('.profile'));
+            }
+            $('.resume').addClass('hide');
+            $('.contact').addClass('hide');
+        }
+    });
+    $('._contact').click(function() {
+        if (!inTransition) {
+            if($('.resume').hasClass('hide')){
+            	$('.contact').removeClass('hide');
+            } else {
+        		inTransition = true;
+            	unrenderResume($('.contact'));
+            }
+            $('.profile').addClass('hide');
+            $('.resume').addClass('hide');
+        }
+    });
+    $('._resume').click(function() {
+        if (!inTransition && $('.resume').hasClass('hide')) {
+        	inTransition = true;
+            $('.profile').addClass('hide');
+            $('.contact').addClass('hide');
+            renderResume();
+        }
+    });
+});
+
+function renderResume() {
+    $('.header').addClass('reduce');
+    setTimeout(function() {
+        $('.container').addClass('widthIncrease');
+        setTimeout(function() {
+            $('.resume').removeClass('hide');
+        	inTransition = false;
+        }, 1000);
+    }, 1000);
+}
+
+function unrenderResume($widget) {
+    $('.container').removeClass('widthIncrease');
+    setTimeout(function() {
+        $('.header').removeClass('reduce');
+        setTimeout(function() {
+            $widget.removeClass('hide');
+        	inTransition = false;
+        }, 1000);
+    }, 1000);
+}
